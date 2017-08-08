12c7ca09d57b5eb2b9334921226da3a61f7a54ba
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2017-08-08 20:36:13 +0300

    Kapt: Do not use awkward ('$' -> '/') name mangling logic for KAPT3 class builder mode (partially fixes KT-19433)
    Before this commit, internal names for nested classes were written as test/Foo/Bar (comparing to test/Foo$Bar in the normal mode), as getting qualified names from such internal names was trivial. But, because of IC, we needed to write class files to the disk, so our decompiler could find such "broken" classes and read it in a wrong way.

--- plugins/kapt3/src/org/jetbrains/kotlin/kapt3/javac/KaptTreeMaker.kt
+++ plugins/kapt3/src/org/jetbrains/kotlin/kapt3/javac/KaptTreeMaker.kt
@@ -16,28 +16,34 @@
 
 package org.jetbrains.kotlin.kapt3.javac
 
+import com.intellij.psi.JavaPsiFacade
+import com.intellij.psi.PsiClass
+import com.intellij.psi.search.GlobalSearchScope
 import com.sun.tools.javac.code.TypeTag
 import com.sun.tools.javac.tree.JCTree
 import com.sun.tools.javac.tree.TreeMaker
 import com.sun.tools.javac.util.Context
+import com.sun.tools.javac.util.Name
 import com.sun.tools.javac.util.Names
 import org.jetbrains.kotlin.codegen.AsmUtil
+import org.jetbrains.kotlin.kapt3.KaptContext
 import org.jetbrains.org.objectweb.asm.Type
 import org.jetbrains.org.objectweb.asm.Type.*
+import org.jetbrains.org.objectweb.asm.tree.ClassNode
 
-class KaptTreeMaker(context: Context) : TreeMaker(context) {
-    val nameTable = Names.instance(context).table
+class KaptTreeMaker(context: Context, private val kaptContext: KaptContext<*>) : TreeMaker(context) {
+    val nameTable: Name.Table = Names.instance(context).table
 
     fun Type(type: Type): JCTree.JCExpression {
         convertBuiltinType(type)?.let { return it }
         if (type.sort == ARRAY) {
             return TypeArray(Type(AsmUtil.correctElementType(type)))
         }
-        return FqName(type.className)
+        return FqName(type.internalName)
     }
 
     fun FqName(internalOrFqName: String): JCTree.JCExpression {
-        val path = internalOrFqName.replace('/', '.').convertSpecialFqName().split('.')
+        val path = getQualifiedName(internalOrFqName.replace('/', '.')).convertSpecialFqName().split('.')
         assert(path.isNotEmpty())
         if (path.size == 1) return SimpleName(path.single())
 
@@ -46,6 +52,43 @@ class KaptTreeMaker(context: Context) : TreeMaker(context) {
             expr = Select(expr, name(path[index]))
         }
         return expr
+    }
+
+    fun getQualifiedName(type: Type) = getQualifiedName(type.internalName)
+
+    fun getSimpleName(clazz: ClassNode) = getQualifiedName(clazz.name).substringAfterLast('.')
+
+    fun getQualifiedName(internalName: String): String {
+        val nameWithDots = internalName.replace('/', '.')
+        // This is a top-level class
+        if ('$' !in nameWithDots) return nameWithDots
+
+        // Maybe it's in our sources?
+        val classFromSources = kaptContext.compiledClasses.firstOrNull { it.name == internalName }
+        if (classFromSources != null) {
+            // Get inner class node pointing to the outer class
+            val innerClassNode = classFromSources.innerClasses.firstOrNull { it.name == classFromSources.name }
+            return innerClassNode?.let { getQualifiedName(it.outerName) + "." + it.innerName } ?: nameWithDots
+        }
+
+        // Search in the classpath
+        return tryToFindNestedClass(nameWithDots)?.qualifiedName ?: return nameWithDots
+
+    }
+
+    private fun tryToFindNestedClass(nameWithDots: String): PsiClass? {
+        assert('$' in nameWithDots)
+        val (outerClassName, nestedClassNames) = nameWithDots.split('$').let { it.first() to it.drop(1) }
+        assert(nestedClassNames.isNotEmpty())
+
+        val project = kaptContext.project
+        var clazz = JavaPsiFacade.getInstance(project).findClass(outerClassName, GlobalSearchScope.allScope(project)) ?: return null
+
+        for (nestedClassName in nestedClassNames) {
+            clazz = clazz.findInnerClassByName(nestedClassName, false) ?: return null
+        }
+
+        return clazz
     }
 
     private fun String.convertSpecialFqName(): String {
@@ -57,7 +100,7 @@ class KaptTreeMaker(context: Context) : TreeMaker(context) {
         return this
     }
 
-    fun convertBuiltinType(type: Type): JCTree.JCExpression? {
+    private fun convertBuiltinType(type: Type): JCTree.JCExpression? {
         val typeTag = when (type) {
             BYTE_TYPE -> TypeTag.BYTE
             BOOLEAN_TYPE -> TypeTag.BOOLEAN
@@ -75,11 +118,11 @@ class KaptTreeMaker(context: Context) : TreeMaker(context) {
 
     fun SimpleName(name: String): JCTree.JCExpression = Ident(name(name))
 
-    fun name(name: String) = nameTable.fromString(name)
+    fun name(name: String): Name = nameTable.fromString(name)
 
     companion object {
-        internal fun preRegister(context: Context) {
-            context.put(treeMakerKey, Context.Factory<TreeMaker>(::KaptTreeMaker))
+        internal fun preRegister(context: Context, kaptContext: KaptContext<*>) {
+            context.put(treeMakerKey, Context.Factory<TreeMaker> { KaptTreeMaker(it, kaptContext) })
         }
     }
 }
---
+++ plugins/kapt3/testData/converter/nestedClasses2.kt
@@ -0,0 +1,35 @@
+// FILE: JavaClass.java
+class JavaClass {
+    class Foo {
+        class Bar {}
+    }
+}
+
+// FILE: a.kt
+interface IFoo {
+    interface IBar {
+        annotation class Anno(vararg val value: kotlin.reflect.KClass<*>)
+
+        @Anno(IZoo::class)
+        interface IZoo
+    }
+}
+
+class Experiment {
+    annotation class Type
+
+    @Type
+    data class Group(s: String)
+}
+
+class Foo {
+    open class Bar {
+        object Zoo
+    }
+}
+
+@IFoo.IBar.Anno(IFoo.IBar.IZoo::class, Foo.Bar::class)
+class Test1(val zoo: Foo.Bar.Zoo) : Foo.Bar(), IFoo.IBar, IFoo.IBar.IZoo {
+    fun a(): Thread.State = Thread.State.NEW
+    fun b(foo: JavaClass.Foo, bar: JavaClass.Foo.Bar) {}
+}
